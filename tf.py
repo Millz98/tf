8e6b05e743ac68ec1a17a1b496b212f51360fa33
Millz98 <dave_m86@hotmail.com>
2023-11-30 18:47:31 -0600

    basic training coded

--- tf.py
+++ tf.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 from keras import layers, models
-from keras.optimizers import Adam
 from keras.datasets import mnist
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications import MobileNet
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,35 +14,19 @@ test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
 train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=10)
 test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=10)
 
-# Replicate single channel to create three channels
-train_images_rgb = np.concatenate([train_images, train_images, train_images], axis=-1)
-test_images_rgb = np.concatenate([test_images, test_images, test_images], axis=-1)
-
-# Create a more advanced CNN model using MobileNet
-base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
-
-# Resize images to match the MobileNet input shape
-resized_train_images_rgb = tf.image.resize(train_images_rgb, (32, 32))
-resized_test_images_rgb = tf.image.resize(test_images_rgb, (32, 32))
-
+# Create a simple CNN model
 model = models.Sequential()
-
-# Add the MobileNet base model
-model.add(base_model)
-
-# Add custom dense layers with dropout and batch normalization
-model.add(layers.GlobalAveragePooling2D())
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.Dropout(0.5))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 
-# Compile the model with a lower learning rate
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-              loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Compile the model with the legacy Adam optimizer
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Adjust data augmentation input shape
 datagen = ImageDataGenerator(
@@ -58,13 +40,19 @@ datagen = ImageDataGenerator(
 )
 
 # Train the model with data augmentation
-history = model.fit(datagen.flow(resized_train_images_rgb, train_labels, batch_size=32),
-                    steps_per_epoch=len(train_images_rgb) / 32, epochs=20,
-                    validation_data=(resized_test_images_rgb, test_labels))
+history = model.fit(datagen.flow(train_images, train_labels, batch_size=32),
+                    steps_per_epoch=len(train_images) / 32, epochs=10,
+                    validation_data=(test_images, test_labels))
 
 # Evaluate the model
-test_loss, test_acc = model.evaluate(resized_test_images_rgb, test_labels)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
 print(f'Test accuracy: {test_acc}')
+
+# Save model
+model.save('my_model.h5')
+
+# Load model
+loaded_model = tf.keras.models.load_model('my_model.h5')
 
 # Plot training history
 plt.plot(history.history['accuracy'], label='accuracy')
